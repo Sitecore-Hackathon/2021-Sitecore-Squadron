d54f593daddda565cf0d977a36ab2b221d0cfda6
PERFICIENT\zach.gay <zach.gay@perficient.com>
2021-03-06 01:26:30 -0500

    Implementation for custom dropdown field type that will display the available rendering variants to select from based on the selected rendering in the custom parent item.

---
+++ SitecoreSquadron.Foundation.Controls/Controls/VariantSelectionControl.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Sitecore.Data;
+using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+using Sitecore.Web.UI.HtmlControls;
+namespace SitecoreSquadron.Foundation.Controls.Controls
+{
+    public class VariantSelectionControl : Control
+    {
+        protected override void DoRender(System.Web.UI.HtmlTextWriter output)
+        {
+            var associatedVariants = GetAssociatedVariants();
+            var keyValuePairs = associatedVariants.Any()
+                ? associatedVariants.Select(i => new KeyValuePair<string, string>(i.Name, i.ID.ToString())).ToList()
+                : new List<KeyValuePair<string, string>>();
+            output.Write("<select" + ControlAttributes + ">");
+            foreach (var keyValuePair in keyValuePairs)
+            {
+                if (keyValuePair.Value == Value)
+                {
+                    output.Write($"<option value=\"{keyValuePair.Value}\" selected=\"selected\">{keyValuePair.Key}</option>");
+                }
+                else
+                {
+                    output.Write($"<option value=\"{keyValuePair.Value}\">{keyValuePair.Key}</option>");
+                }
+            }
+            output.Write("</select>");
+        }
+        protected override bool LoadPostData(string value)
+        {
+            if (value == null)
+                return false;
+            if (this.GetViewStateString("Value") != value)
+                Sitecore.Context.ClientPage.Modified = true;
+            this.SetViewStateString("Value", value);
+            return true;
+        }
+        private IEnumerable<Item> GetAssociatedVariants()
+        {
+            var renderingSettingItem = GetRenderingSettingItem();
+            var selectedRenderingName = new ReferenceField(renderingSettingItem.Fields[Templates.LocalizedRenderingVariantSetting.Fields.RenderingVariantId]).TargetItem.Name;
+            var renderingVariantDefinitionsInSite = GetSiteRenderingVariantDefinitions(renderingSettingItem);
+            return renderingVariantDefinitionsInSite.FirstOrDefault(r => r.Name.Equals(selectedRenderingName)).Children;
+        }
+        private Item GetRenderingSettingItem()
+        {
+            var currentItemRegex = new Regex(@"\{(.*?)\}");
+            return Sitecore.Context.ContentDatabase.GetItem(new ID(currentItemRegex.Match(ControlAttributes).Value)).Parent;
+        }
+        private IEnumerable<Item> GetSiteRenderingVariantDefinitions(Item renderingSettingItem)
+        {
+            while (!renderingSettingItem.TemplateID.ToString().Equals(Templates.Site.TemplateId))
+            {
+                renderingSettingItem = renderingSettingItem.Parent;
+            }
+            var presentationItem = renderingSettingItem.Children.FirstOrDefault(c => c.TemplateID.ToString().Equals(Templates.Presentation.TemplateId));
+            var variantsGrouping = presentationItem.Children.FirstOrDefault(c => c.TemplateID.ToString().Equals(Templates.VariantsGrouping.TemplateId));
+            return variantsGrouping.Children;
+        }
+    }
+}
---
+++ SitecoreSquadron.Foundation.Controls/Templates.cs
@@ -0,0 +1,25 @@
+﻿public struct Templates
+{
+    public struct LocalizedRenderingVariantSetting
+    {
+        public struct Fields
+        {
+            public const string RenderingVariantId = "{8842AEB5-EC08-41AF-A027-824439357B82}";
+        }
+    }
+
+    public struct Site
+    {
+        public const string TemplateId = "{FE505F0A-D6E4-446C-98D5-5F4B633DAF1C}";
+    }
+
+    public struct Presentation
+    {
+        public const string TemplateId = "{26BF5BE6-815D-4078-98BF-AB66B82D2171}";
+    }
+
+    public struct VariantsGrouping
+    {
+        public const string TemplateId = "{BBB9AE71-C6B4-4201-83E7-AC5B3EF373DC}";
+    }
+}
